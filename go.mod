4a5cdf85e11ec02b40fcc348bd0d75092995f1c7
thxrhmn <rahmanmuhaemin@gmail.com>
2023-05-06 06:30:27 +0700

    change mysql to postgres

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/midtrans/midtrans-go v1.3.6
 	golang.org/x/crypto v0.8.0
-	gorm.io/driver/mysql v1.5.0
+	gorm.io/driver/postgres v1.5.0
 	gorm.io/gorm v1.25.0
 )
 
@@ -18,10 +18,12 @@ require (
 	github.com/creasty/defaults v1.5.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -3,9 +3,10 @@ package mysql
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -20,17 +21,20 @@ func DatabaseInit() {
 	// dsn := "root:@tcp(localhost:3306)/week-02-task-666?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	dsn := "root:@tcp(localhost:3306)/holyways?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// // mysql
+	// dsn := "root:@tcp(localhost:3306)/holyways?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	// var DB_HOST = os.Getenv("DB_HOST")
-	// var DB_USER = os.Getenv("DB_USER")
-	// var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	// var DB_NAME = os.Getenv("DB_NAME")
-	// var DB_PORT = os.Getenv("DB_PORT")
+	// // postgres
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
-	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// // postgress
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
